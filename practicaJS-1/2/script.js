7985921688d3cea16db9d79024ff0697ab8d7567
Lautaro Rocha <lautarovrocha@gmail.com>
2023-06-30 19:12:18 -0300

    one solved

--- practicaJS-1/2/script.js
+++ practicaJS-1/2/script.js
@@ -1,6 +1,7 @@
 ////escribí una función calcularAñosPerro()
 ///que tome dos parametros (nombre y añoDeNacimiento)
-///y muestre por consola un mensaje 'nombre tiene X años en años de humano, y X años en años de perro'
+///y muestre por consola un mensaje 'nombre tiene X años
+///en años de humano, y X años en años de perro'
 //se dice que un año humano equivale a 7 años de perro
 //si los parámetros no son correctos, la función debe cortar
 //su ejecución y devolver un mensaje de error
@@ -16,6 +17,24 @@
 //y devuelva a una variable el primer elemento del array
 //donde clave === valor
 
+class Friend{
+    constructor(name, job){
+        this.name = name;
+        this.job = job;
+    }
+    goToWork(){
+        console.log(`Soy ${this.name} y trabajo de ${this.job}`)
+    }
+}
+
+
+const friends = [ 
+                new Friend('Raúl', 'albañil'), 
+                new Friend('Sofía', 'programadora'),
+                new Friend('Julieta', 'tester'),
+                new Friend('David', 'mozo'),
+                new Friend('Julián', 'SRE')
+]
 
 
 //escribi una función returnLongest() que tome como parametros dos string
@@ -28,3 +47,7 @@
 //escribí una función countVowels() que reciba un string
 //y devuelva ( decidí vos como mostrar el resultado )
 //la cantidad de vocales en la palabra
+
+
+///escribir una función que tome como parámetro
+//
