eefbcf0bbe1b0e8f48a3207ccd99811db9db10d9
Lautaro Rocha <lautarovrocha@gmail.com>
2023-07-04 20:28:42 -0300

    DOM-JS- al fin !

--- 1b/JS/clase-cuatro/script.js
+++ 1b/JS/clase-cuatro/script.js
@@ -5,33 +5,115 @@
 
 //modos de acceso
 
-//getElementsByTagName, getElementsByClassName. byName
+///los metodos de acceso al DOM que incluyen las palabras
+//'ElementSSS' o 'All', devuelven un array con todos 
+//los elementos que matchean al parametro
+//getElementsByTagName, querySelectorAll
 
-//getElementById
+//getElementById devuelve el primer nodo que tiene un
+//atributo ID que matchea con el parámetro
+//getElementsByClassName nos devuelve un array de nodos que matchean su clase con el parámetro
 
-//querySelector, selectorAll
-    //por id
-    //por clase
-    //por nombre de tag
+//los método más poderosos para acceder a los nodos del DOM son querySelector y 
+//querySelectorAll, que toman
+//como parámetro un selector con la sintaxis de CSS
+const openButton = document.getElementById('open-btn');
+const addButton = document.querySelector('#add-btn')
+//por id
+const removeButton = document.querySelector('#rmv-btn')
+//por clase
+const popUp = document.querySelector('.pop-up')
+//por nombre de tag
+const allButtons = document.querySelectorAll('button')
 
-//createElement
+//el metodo createElement de la interfaz document, me deja crear nuevos nodos en el documento que quedan almacenados en memoria hasta que sean 'appendeados' o agregados al documento / nodo.
+const newElement = document.createElement('p')
 
-//appendChild()
+//con el método appendChild() podemos añadir nuevos hijos a un NODO DOM.
 
-///textContent, innerHTML
+const main = document.querySelector('main')
+
+function addElementToMain(ele){
+    main.appendChild(ele)
+}
+
+///las propiedades textContent e innerHTML nos dejan añadir contenido al nodo-texto de cada nodo DOM
+
+///la propiedad innerHTML me deja escribir como un string código HTML que se agrega adentro del nodo
+//ocupa el mismo espacio que textContent, por lo que se sobreescriben entre ellas
+newElement.innerHTML = '<button>BOTÖN</button>'
+newElement.textContent = '<button>BOTÖN</button>'
 
 
+const newInput = document.createElement('input')
+
+//para cambiar/asignar los atributos de algún nodo
+//usamos el método setAttribute(), que toma como parámetros primero el atributo, y luego el valor
+//este metodo deja el atributo con el valor especificado
+//sin añadirvalores preexistentes
+newInput.setAttribute('type', 'date')
+newInput.setAttribute('class', 'big')
 
 
-//setAttribute()
+//el método classList me devuelve un array con
+//todas las clases aplicadas sobre un nodo
+const inputClasses = newInput.classList
+//puede añadir o remover valores de esas clases
+inputClasses.add('yellow')
+inputClasses.remove('yellow')
+
+// function addParagraphToMain(){
+    // const elementToAppend = document.createElement('p')
+    // elementToAppend.innerText = 'Este parráfo lo creé con JS'
+    // main.appendChild(elementToAppend) 
+// }
+
+const addParagraphToMain = () => {
+    const elementToAppend = document.createElement('p')
+    elementToAppend.innerText = 'Este parráfo lo creé con JS'
+    main.appendChild(elementToAppend) 
+}
 
 
-//classList 
+//hay dos sintaxis para detectar eventos
+//y asociarlos a la invocación de una función
 
+//primero, podemos usar el método addEventListener
+//que toma como primer parametro el nombre de un evento y como segundo la función a ejecutar cuando se detecte ese evento
+addButton.addEventListener('click', addParagraphToMain)
 
-//add paragraph function
+//esta es la sintaxis más moderna, en la que accedo
+//a la propiedad on+nombreDeEvento y la igualo a una función
+removeButton.onclick = () => {
+    //accedo al ultimo hijo del nodo
+    const lastP = main.lastChild
+    //lo remuevo
+    main.removeChild(lastP)
+};
 
-//event listeners
+// openButton.onclick = () => {
+//     const currentText = openButton.textContent.trim()
+//     console.log(currentText)
+//     if(currentText === 'Abrir'){
+//         //cambiar texto de botón
+//         openButton.textContent = 'Cerrar'
+//         //abrir popup 
+//         popUp.classList.toggle('closed')
+//     }else{
+//         //cambiar texto del botón
+//         openButton.textContent = 'Abrir'
+//         //cerrar popup
+//         popUp.classList.toggle('closed')
+//     }
+// }
 
+//un rework para una sintaxis 
+//más resumida y moderna
+openButton.onclick = () => {
+    //desestrucuración de un objeto
+    const { textContent } = openButton
+    textContent.trim() === 'Abrir' ? openButton.textContent = 'Cerrar' : openButton.textContent = 'Abrir';
+    popUp.classList.toggle('closed')
+}
 
 
--- 3b/JS/clase-cuatro/script.js
+++ 3b/JS/clase-cuatro/script.js
@@ -1,34 +1,168 @@
-//el DOM es una representación en árbol de nodos
+//el DOM (Document Object Model)es una representación en árbol de nodos
 //del documento HTML. La DOM API del navegador
 //nos ofrece varios metodos para acceder y modificar 
 //dichos nodos.
 
 //modos de acceso
-
 //getElementsByTagName, getElementsByClassName. byName
 
-//getElementById
 
-//querySelector, selectorAll
+///el metodo getElementByTaGName devuelve un arreglo de todos los elementos del DOM que matchean su etiqueta con el parámetro
+
+//buggy
+const buttons = document.getElementsByTagName('main')
+const buttonClassButtons = document.getElementsByClassName('btn')
+
+//getElementById
+const addButton = document.getElementById('add-btn')
+
+
+//querySelector, querySelectorAll ===> devuelve un array de nodos
     //por id
     //por clase
     //por nombre de tag
+    //para guardar un nodo de DOM en una variable,
+    //la iniciamos y le damos el valor de document.metodoDeAcceso
 
-//createElement
-
-//appendChild()
-
-///textContent, innerHTML
-
-
-
-
-//setAttribute()
+    //querySelector toma como parametro un selector en la sintaxis de CSS
+const openButton = document.querySelector('#open-btn')
+const removeButton = document.querySelector('#rmv-btn')
+const popUp = document.querySelector(".pop-up")
+const main = document.querySelector('main')
 
 
-//classList 
+//createElement es un método de la interfaz document
+//que permite crear nodos de DOM que son almacenados en memoria
+//toma como parametro un tagname (html)
 
+const newInput = document.createElement('input')
+const newParagraph = document.createElement('p')
+
+//appendChild() es un método de los NODOS del DOM
+//que nos permite añadir como hijo suyo un elemento
+
+// main.appendChild(newInput)
+
+
+///textContent, innerHTML, 
+//innerText
+    newParagraph.textContent = 'Este parráfo lo creé con JS'
+  //newParagraph.textContent = '<button>BOTÖN</button>'
+//inner HTML agrega como hijo unnodo creado a partir de un string describiendo HTML
+ ///  newParagraph.innerHTML = '<button>BOTÖN</button>'
+
+
+// main.appendChild(newParagraph)
+
+
+//setAttribute() es un método de los nodos del DOM
+//que me permite setear atributos a las etiquetas html.
+//toma dos parámetros: el nombre del attr, y el valor
+
+newInput.setAttribute('type', 'password')
+newInput.setAttribute('id', 'password-input')
+
+newInput.setAttribute('class', 'big')
+
+//classList es una PROPIEDAD de los NODOS del DOM
+//que devuelve una lista de todas las clases que tiene
+//el elemento y expone los métotodos add y remove
+const classes = newInput.classList
+
+classes.add('yellow')
+classes.add('white')
+classes.remove('big')
 
 //add paragraph function
 
+// function addParagraphToMain(){
+    //crear elemento
+    //const newElement = document.createElement('p');
+    //escribir en el elemento
+    //newElement.textContent = JS_TEXT;
+    //añadir elemento
+    //main.appendChild(newElement)
+// }
+
+const JS_TEXT = 'Este texto fue generado por JS'
+
+const addParagraphToMain = () => {
+    //crear elemento
+    const newElement = document.createElement('p');
+    //escribir en el elemento
+    newElement.textContent = JS_TEXT;
+    //añadir elemento
+    main.appendChild(newElement)
+}
+
+const removeLastParagraphFromMain = () => {
+            //la propiedad lastChild de un NODO
+            //guarda su último hijo
+    const last = main.lastChild
+            //el método removeChild oma un nodo como
+            //parametro y lo elimina del DOM
+    main.removeChild(last)
+}
+
 //event listeners
+
+///hay dos maneras de asociar un nodo a una función 
+//para que se ejecute cuando se detecte X evento
+
+///la primera es usanod el método de nodo addEventListener, pasando como parámetros el nombre del evento y la función a ejectuar
+
+addButton.addEventListener('click', addParagraphToMain)
+
+//la otra sintaxis es entrar a la propiedad onNombreDeEvento de un nodo e igualarla a una función con nombre o anónima}
+
+removeButton.onclick = () => {
+        //la propiedad lastChild de un NODO
+        //guarda su último hijo
+    const last = main.lastChild
+        //el método removeChild oma un nodo como
+        //parametro y lo elimina del DOM
+    main.removeChild(last)
+}
+
+
+///popup function
+/*
+openButton.onclick = () => {
+    //cambiar el texto del botón según corresonda al estado del popup
+    const current = openButton.textContent;
+
+    if(current.trim() === 'Abrir'){
+        //abrir popup  y cambiar texto
+        openButton.textContent = 'Cerrar'
+        popUp.classList.remove('closed')
+    }else{
+        openButton.textContent = 'Abrir'
+        popUp.classList.add('closed')
+        //cerrar popup y cambiar texto
+    }
+
+}*/
+
+openButton.onclick = () => {
+    //destructuración de objeto
+    const {textContent} = openButton;
+
+    popUp.classList.toggle('closed')
+
+    //operador ternario
+    textContent.trim() === 'Abrir' ?  openButton.textContent = 'Cerrar' : openButton.textContent = 'Abrir'
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
