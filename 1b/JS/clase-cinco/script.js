f2bce17575ff86bacd62fd56282f266e94a8a234
Lautaro Rocha <lautarovrocha@gmail.com>
2023-07-05 18:24:58 -0300

    today's goal

---
+++ 1b/JS/clase-cinco/script.js
@@ -0,0 +1,7 @@
+///a partir del formulario, generar una instancia de la clase
+//Contact, añadirla al fin del array,y actualizar el DOM para que 
+//sincronizado con el array PHONEBOOK
+
+class Contact {}
+
+const PHONEBOOK = [];
