c7d1cb6354538ada50d51b0d3814c060b0e6914d
Lautaro Rocha <lautarovrocha@gmail.com>
2023-07-06 17:34:09 -0300

    class 5 setupr

---
+++ 3b/JS/clase-cinco/script.js
@@ -0,0 +1,16 @@
+///a partir del formulario, generar una instancia de la clase
+//Contact, añadirla al fin del array,y actualizar el DOM para que esté
+//sincronizado con el array PHONEBOOK
+
+class Contact {
+
+}
+
+const PHONEBOOK = [];
+///crear el contacto
+
+//enviarlo al array
+
+
+
+//actualizar el dom
