69057c14ff213dee8eebaff3e2c49f7bad5dab63
Lautaro Rocha <lautarovrocha@gmail.com>
2023-07-13 20:17:56 -0300

    clase siete- terminamos la agenda !!!

--- 3b/JS/clase-siete/script.js
+++ 3b/JS/clase-siete/script.js
@@ -41,6 +41,8 @@ class Contact {
 
 const STORAGE_KEY = 'PHONEBOOK';
 
+// let newName, newEmail, newTel;
+
 const PHONEBOOK = [];
 
 const nameInput = document.querySelector('#name')
@@ -48,6 +50,13 @@ const telInput = document.querySelector('#tel')
 const emailInput = document.querySelector('#mail')
 const addBtn = document.querySelector('#add-btn')
 const phonebookDOM = document.querySelector('#agenda') 
+
+
+// const newNameInput = document.querySelector('#new-name')
+// const newTelInput = document.querySelector('#new-tel')
+// const newEmailInput = document.querySelector('#new-email')
+// const confirmEditBtn = document.querySelector('#confirm-edit')
+// const popUpEdit = document.querySelector('.pop-up-edit')
 
 const checkIfContactAlreadyExists = ( contact ) => {
     if( PHONEBOOK.some( ctct => ctct.email === contact.email  ) ){
@@ -96,11 +105,68 @@ const createContactDiv = ( contact ) => {
     const contactTel = document.createElement('span')
     contactTel.textContent  = tel;
 
+    const editBtn = document.createElement('button')
+    editBtn.classList.add('edit-btn')
+    editBtn.textContent = 'Editar'
+    const deleteBtn = document.createElement('button')
+    deleteBtn.classList.add('delete-btn')
+    deleteBtn.textContent = 'Borrar'
+
+    editBtn.addEventListener('click', editContact)
+    deleteBtn.addEventListener('click', deleteContact)
+
     newContact.appendChild(contactName)
+    newContact.appendChild(editBtn)
     newContact.appendChild(contactMail)
+    newContact.appendChild(deleteBtn)
     newContact.appendChild(contactTel)
 
     phonebookDOM.appendChild(newContact)
+}
+
+//editar contacto
+const editContact = (e) => {
+    ///target es una propiedad de la interfaz Evento que hace referencia al nodo del DOM donde se detectó el mismo
+
+    //nextSiblinbg es una propiedad de los nodos de DOM que hace referencia al elemento hermano a la derecha
+    const selectedEmail = e.target.nextSibling.textContent
+
+    ///encontrar el contacto seleccionado
+    const selectedContact = PHONEBOOK.find( contacto => contacto.email === selectedEmail)
+    const {name, tel, email} = selectedContact;
+    // popUpEdit.classList.remove('closed')
+
+    ///conseguir los nuevos valores
+    const {newName, newTel, newMail} = getNewValues()
+
+    ///editarlo
+    const selectedIndex = PHONEBOOK.indexOf(selectedContact)
+    ///guardarlo
+    PHONEBOOK[selectedIndex].name = newName ? newName : name;
+    PHONEBOOK[selectedIndex].tel = newTel ? newTel : tel ;
+    PHONEBOOK[selectedIndex].email = newMail ? newMail : email;
+
+    //actualizar el storage
+    //actualizar el dom
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(PHONEBOOK))
+    updateDOM()
+}
+
+
+//borrar contacto
+const deleteContact = (e) => {
+    const selectedEmail = e.target.previousSibling.textContent
+    const selectedContact = PHONEBOOK.find( contacto => contacto.email === selectedEmail)
+    const selectedIndex = PHONEBOOK.indexOf(selectedContact)
+
+    const confirmation = confirm(`¿Segurx que quieres eliminar a ${selectedContact.name} de tus contactos?`)
+
+    if(confirmation){
+        PHONEBOOK.splice(selectedIndex, 1)
+        localStorage.setItem(STORAGE_KEY, JSON.stringify(PHONEBOOK))
+        updateDOM()
+    }
+    
 }
 
 addBtn.addEventListener('click', createContact)
@@ -118,3 +184,28 @@ window.onload = () => {
     checkStorage()
 };
 
+// confirmEditBtn.onclick = (e) => {
+//     getValuesFromEditPopUp(e)
+// }
+
+
+// function getValuesFromEditPopUp(e) {
+//     e.preventDefault()
+//     popUpEdit.classList.add('closed')
+
+//     newName = newNameInput.value
+//     newTel =  newTelInput.value
+//     newEmail = newEmailInput.value
+// }
+
+function getNewValues(){
+    const newName = prompt('Nuevo nombre')
+    const newMail = prompt('Nuevo mail')
+    const newTel = prompt('Nuevo telefono') 
+
+    return {
+        newName : newName,
+        newMail : newMail,
+        newTel : newTel
+    }
+}
